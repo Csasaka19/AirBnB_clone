d2eba9be10a9e111016cf44fc615f1e860726ca6
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-11-27 14:39:21 +0000

    number 2

--- models/base_model.py
+++ models/base_model.py
@@ -25,18 +25,23 @@ class BaseModel:
         Args:
         args: variable arguments, unmapped
         kwargs: variable mapped arguments
-
+        """
+        
         if len(kwargs) > 0:
             for key, value in kwargs.items():
+                form = '%Y-%m-%dT%H:%M:%S.%f'
                 if key in ['created_at', 'updated_at']:
-                    self.__dict__[key] = datetime.strptime
-                    (value, '%Y-%m-%dT%H:%M:%S.%f')
-                self.__dict__[key] = value
-        else:"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+                    value = datetime.strptime(value, form)
+                if key != '__class__':
+                    setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+        """
         models.storage.new(self)
+        """
 
     def __str__(self):
         """
